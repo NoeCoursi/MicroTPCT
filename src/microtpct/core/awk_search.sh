31f24046c48e9cc5c0a423e39f8b30d86d6a777e
BasileBergeron <basile.bergeron9z@gmail.com>
2026-01-19 18:02:52 +0100

    + header/peptide_seq info for grep and awk

--- src/microtpct/core/awk_search.sh
+++ src/microtpct/core/awk_search.sh
@@ -3,16 +3,25 @@
 query_file_fasta="$1"
 target_file_fasta="$2"
 
-
 /usr/bin/time -v awk '
 BEGIN {
-    while ((getline < ARGV[1]) > 0)
-        if ($0 !~ /^>/) peptides[$0]=1
-    ARGV[1]=""
+    # Read the query FASTA
+    while ((getline < ARGV[1]) > 0) {
+        if ($0 ~ /^>/) {
+            peptide_header = $0       # store the header
+        } else {
+            peptides[$0] = peptide_header  # map sequence -> header
+        }
+    }
+    ARGV[1] = ""  # clear so next ARGV files are processed normally
 }
-/^>/ {header=$0; next}
+
+# Process target FASTA
+/^>/ {target_header = $0; next}  # store current target header
 {
-    for (p in peptides)
-        if (index($0,p)) print p "\t" header
+    for (seq in peptides)
+        if (index($0, seq))  # if peptide seq is found in target
+            print seq "\t" peptides[seq] "\t" target_header
 }
-' "$query_file_fasta" "$target_file_fasta" | sort 
+' "$query_file_fasta" "$target_file_fasta" | sort
+
