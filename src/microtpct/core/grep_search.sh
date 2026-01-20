c1938faa4e550b44c6f3de0ec125d9c4717b8e0a
BasileBergeron <basile.bergeron9z@gmail.com>
2026-01-20 17:29:01 +0100

    New awk/grep functionnal script

--- src/microtpct/core/grep_search.sh
+++ src/microtpct/core/grep_search.sh
@@ -1,17 +1,50 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-query_file_fasta="$1"
-target_file_fasta="$2"
+set -eu
 
-echo "____________________________________________________________________________________________________________________________"
-echo "Matched"
-echo "____________________________________________________________________________________________________________________________"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 fasta1 fasta2" >&2
+    exit 1
+fi
 
-grep -F -o -f "$query_file_fasta" "$target_file_fasta" | LC_ALL=C sort | uniq -c
-echo "____________________________________________________________________________________________________________________________"
-echo "Non matched"
-echo "____________________________________________________________________________________________________________________________"
+FASTA1=$1
+FASTA2=$2
 
-matched=$(grep -F -o -f "$query_file_fasta" "$target_file_fasta" | sort -u)
-grep -F -x -v -f <(echo "$matched") "$query_file_fasta" | grep -v ">" | sort | uniq -c
-echo "____________________________________________________________________________________________________________________________"
+
+TMP_FASTA2=$(mktemp)
+trap 'rm -f "$TMP_FASTA2"' EXIT
+
+LC_ALL=C awk '
+  /^>/ {
+    if (seq) print header "\t" seq
+    header=substr($0,2)
+    seq=""
+    next
+  }
+  { seq=seq $0 }
+  END { if (seq) print header "\t" seq }
+' "$FASTA2" > "$TMP_FASTA2"
+
+echo "query_header,target_header,position"
+
+
+LC_ALL=C awk '
+  /^>/ {
+    if (seq) print header "\t" seq
+    header=substr($0,2)
+    seq=""
+    next
+  }
+  { seq=seq $0 }
+  END { if (seq) print header "\t" seq }
+' "$FASTA1" |
+while IFS=$'\t' read -r h1 seq1; do
+
+    grep -F "$seq1" "$TMP_FASTA2" |
+    awk -F'\t' -v q="$h1" -v s="$seq1" '
+      {
+        pos=index($2, s)-1
+        if(pos>=0) print q","$1","pos
+      } END { if(NR==0) print q",," }'
+
+done
