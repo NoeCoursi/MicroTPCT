09000a2851be9ee4f8257a607ddba114ee080b7d
BasileBergeron <basile.bergeron9z@gmail.com>
2026-01-20 18:10:52 +0100

    changed output but extremely slow

--- src/microtpct/core/boyer_moore.py
+++ src/microtpct/core/boyer_moore.py
@@ -1,58 +1,42 @@
-#/usr/bin/python3
-# Required pip install pybmoore
+#!/usr/bin/env python3
 
-import pyfastx #type: ignore
-import pybmoore #type: ignore
 import sys
+import csv
+import pybmoore
 
-
-from collections import defaultdict
 from Bio import SeqIO #type: ignore
 
 
-query_path = sys.argv[1]
-target_path = sys.argv[2]
 
-
-def match_query_to_target(
-        query_path: str,
-        target_path: str) -> dict:
-    
-    """
-    Takes in argument a peptide file.fa and a target file.fa
-
-    usage python3 boyer_moore query.fa target.fa
-
-    returns : 
-        matching_dict = {
-            "matched": [],
-            "non_matched": []
-        }
-
-    """
-        
-    matching_dict = {
-        "matched": [],
-        "non_matched": []
-    }
-
-    # Boyer Moore est plus optimisé pour des grandes séquences
-    full_target = "".join(str(record.seq) for record in SeqIO.parse(target_path, "fasta"))
-
-
-    with open(query_path) as fh:
-        for record in SeqIO.parse(fh, "fasta"):
-            print(record.id)
-            matches = pybmoore.search(str(record.seq), full_target)
-            if len(matches) > 0 :
-                matching_dict["matched"].append(record.id)
-            else :
-                matching_dict["non_matched"].append(record.id)
+def main(query, target):
+    query_fasta = sys.argv[1]
+    target_fasta = sys.argv[2]
 
 
 
+    output_file = "output.csv"
+    with open(output_file, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        writer.writerow(["query_header", "target_header", "position"])
 
-matching_dict = match_query_to_target(query_path, target_path)
+        for qrec in SeqIO.parse(query_fasta, "fasta"):
+            print(f"{qrec.id} / 200")
+            found_match = False
+            for trec in SeqIO.parse(target_fasta, "fasta"):
+                # pybmoore.search retourne liste de (start, end) pour tous les matches
+                matches = pybmoore.search(str(qrec.seq),str(trec.seq))
+                if matches:
+                    for start, _ in matches:
+                        writer.writerow([qrec.id, trec.id, start])
+                    found_match = True
+            if not found_match:
+                writer.writerow([qrec.id, "", ""])
 
-print(matching_dict)
+    print(f"CSV généré avec succès : {output_file}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} fasta1 fasta2", file=sys.stderr)
+        sys.exit(1)
+    main(sys.argv[1], sys.argv[2])
 
