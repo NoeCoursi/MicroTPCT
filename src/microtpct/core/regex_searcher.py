6ec165b4a61f9dc6193ec383c3bd4d5dded4828d
BasileBergeron <basile.bergeron9z@gmail.com>
2026-01-22 16:00:04 +0100

    regex search oreinté objet

--- src/microtpct/core/regex_searcher.py
+++ src/microtpct/core/regex_searcher.py
@@ -2,23 +2,18 @@
 
 import sys, re
 import pandas as pd
-
-from Bio import SeqIO  # type: ignore
 from collections import defaultdict
 
-
-query_fasta, target_fasta = sys.argv[1], sys.argv[2]
-results = defaultdict(list)
-
+from microtpct.core.databases import TargetDB, QueryDB
+from microtpct.core.results import Match, MatchResult
 
 
-def get_peptide_set(query_fasta: str) -> dict:
-
+def get_peptide_set(queries_id, queries_seq) -> dict:
     query_length_dict = defaultdict(list)
-    with open(query_fasta, "r") as gfh :
-        for qrec in SeqIO.parse(gfh, "fasta"):
-            query_length_dict[len(qrec.seq)].append((qrec.id,qrec.seq))
+    for q_id, q_seq in zip(queries_id, queries_seq):
+        query_length_dict[len(q_seq)].append((q_id,q_seq))
     return query_length_dict
+
 
 def kmer_set(sequence: str, k: int) -> set:
     """
@@ -42,31 +37,29 @@ def kmer_sets_filter(k_mer_ser, first_aa, last_aa):
     filtered_set = {k for k in k_mer_ser if (k.startswith((first_aa, "X")) and k.endswith((last_aa, "X")))}
     return filtered_set
 
-# dictionnary of peptide through their sequence length
-query_length_dict = get_peptide_set(query_fasta)
+
+def run_regex_search(target_db: TargetDB, query_db: QueryDB) -> MatchResult:
+    # dictionnary of peptide through their sequence length
+    query_length_dict = get_peptide_set(query_db.ids, query_db.ambiguous_il_sequences)
+    matches: list[Match] = []
 
 
-results = defaultdict(list)
-cmp = 0
-with open(target_fasta, "r") as tfh :
-    for trec in SeqIO.parse(target_fasta, "fasta"): # read target fasta sequence per sequence
-        cmp +=1 
-        print(cmp)
+    for t_id, t_seq in zip(target_db.ids, target_db.ambiguous_il_sequences):
+
         for key_len, pep_list in query_length_dict.items():
-            full_kmer_set = kmer_set(trec.seq, key_len) # creates a single k-mer set for every peptide length
+            full_kmer_set = kmer_set(t_seq, key_len) # creates a single k-mer set for every peptide length
             for pep in pep_list:
                 pep_id, pep_seq = pep[0],pep[1]
                 filtered_kmer_set = kmer_sets_filter(full_kmer_set, pep_seq[0],pep_seq[-1]) # filter the kmer set by signel peptide
 
                 for kmer in filtered_kmer_set: # compares every kmer after filtration with the peptide
-                    matches = re.match(str(kmer) , str(pep_seq))
+                    match = re.search(str(kmer) , str(pep_seq))
 
-                    if matches :
-                        results[pep_id].append((trec.id,kmer)) # Keep the results
-                        print(results)
+                    if match :
+                        matches.append(Match(
+                            query_id=pep_id,
+                            target_id=t_id,
+                            position=(match.start(),match.end())))
 
-print(results)
-
-
-
+    return MatchResult(matches)
 
