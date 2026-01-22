99d95765f25d3b5830c31f38c5242c9ea245b68f
BasileBergeron <basile.bergeron9z@gmail.com>
2026-01-22 17:24:48 +0100

    Object and indexed search, functional and tested

--- src/microtpct/core/regex_searcher.py
+++ src/microtpct/core/regex_searcher.py
@@ -8,7 +8,8 @@ from microtpct.core.databases import TargetDB, QueryDB
 from microtpct.core.results import Match, MatchResult
 
 
-def get_peptide_set(queries_id, queries_seq) -> dict:
+# Creat a peptide dictionnary with peptide length as keys
+def get_peptide_dict(queries_id, queries_seq) -> dict:
     query_length_dict = defaultdict(list)
     for q_id, q_seq in zip(queries_id, queries_seq):
         query_length_dict[len(q_seq)].append((q_id,q_seq))
@@ -33,25 +34,25 @@ def kmer_set(sequence: str, k: int) -> set:
         for i in range(len(sequence) - k + 1)
     }
 
-def kmer_sets_filter(k_mer_ser, first_aa, last_aa):
-    filtered_set = {k for k in k_mer_ser if (k.startswith((first_aa, "X")) and k.endswith((last_aa, "X")))}
-    return filtered_set
-
+def kmer_sets_filter(k_mer_set):
+    return {k for k in k_mer_set if "." in k}
 
 def run_regex_search(target_db: TargetDB, query_db: QueryDB) -> MatchResult:
     # dictionnary of peptide through their sequence length
-    query_length_dict = get_peptide_set(query_db.ids, query_db.ambiguous_il_sequences)
+    query_length_dict = get_peptide_dict(query_db.ids, query_db.ambiguous_il_sequences)
     matches: list[Match] = []
 
+    print(query_length_dict)
 
     for t_id, t_seq in zip(target_db.ids, target_db.ambiguous_il_sequences):
 
         for key_len, pep_list in query_length_dict.items():
+
             full_kmer_set = kmer_set(t_seq, key_len) # creates a single k-mer set for every peptide length
+            filtered_kmer_set = kmer_sets_filter(full_kmer_set) # filter the kmer to keep only kmer with wildcards
+
             for pep in pep_list:
                 pep_id, pep_seq = pep[0],pep[1]
-                filtered_kmer_set = kmer_sets_filter(full_kmer_set, pep_seq[0],pep_seq[-1]) # filter the kmer set by signel peptide
-
                 for kmer in filtered_kmer_set: # compares every kmer after filtration with the peptide
                     match = re.search(str(kmer) , str(pep_seq))
 
@@ -63,3 +64,7 @@ def run_regex_search(target_db: TargetDB, query_db: QueryDB) -> MatchResult:
 
     return MatchResult(matches)
 
+
+
+print("\n=== Regex search() ===")
+print(run_regex_search(TargetDB, QueryDB))
