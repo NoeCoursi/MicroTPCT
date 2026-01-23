2744b3d961eb3f13b71ee69bfd95dd42712c2641
AmbreBordasENSAT <ambrebordas2307@gmail.com>
2026-01-23 15:22:24 +0100

    Test of writers script

--- tests/writer.py
+++ tests/writer.py
@@ -1,28 +1,97 @@
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
+import openpyxl
 
-from microtpct.io.writers import write_outputs_to_csv
+from microtpct.io.writers import write_outputs_to_csv 
 
-
-#data = {
-#    "micro_peptide_ID": ["mp_001", "mp_002", "mp_003"],
-#    "protein_ID": ["prot_A", "prot_B", "prot_C"],
-#    "peptide_seq": ["MKTLLV", "AGPQLR", "VVKSTA"],
-#    "J_peptide_seq": ["MKTL-LV", "AGPQ-LR", "VVKS-TA"],
-#    "start_position": [12, 45, 87],
-#}
-
-#df = pd.DataFrame(data)
-# print(df)
+from microtpct.io.validators import validate_peptide_input, validate_protein_input
+from microtpct.io.readers import read_file, SequenceRole
+from microtpct.io.converters import *
 
 
-#write_micropeptides_to_csv(df) #,file_path="C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/MicroTPCT")
-#write_metrics_to_csv(df) #,file_path="C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/MicroTPCT")
+                            
+                            # Test with true outputs of a minimal pipeline
 
 
-                                # Test du merge 
+# Importation of files 
+peptide_file_path = Path("C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/data/Liste_peptides.xlsx")
+proteome_file_path = Path("C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/data/Drosophila Microproteome Openprot 2025-10-09 all conditions_2025-11-24_1613.xlsx")
 
+
+
+def mini_pipeline(path, role: SequenceRole):
+    sequences_iterator = read_file(path, role=role)
+
+    sequences = list(sequences_iterator)
+
+    # # Validate
+    # if role == SequenceRole.PROTEIN:
+    #     for obj in sequences:
+    #         validate_protein_input(obj)
+    # else:
+    #     for obj in sequences:
+    #         validate_peptide_input(obj)
+
+    # Build DB
+    db = build_database(sequences, role=role)
+
+    return db
+
+
+
+query_db = mini_pipeline(peptide_file_path, SequenceRole.PEPTIDE)
+print(query_db.to_dataframe())
+
+target_db = mini_pipeline(proteome_file_path, SequenceRole.PROTEIN)
+print(target_db.to_dataframe())
+
+
+# --> ATTENTION à faire pip install pyahocorasick (module externe) 
+
+# from microtpct.core.match_algorithms.match_find import run_find
+from microtpct.core.match_algorithms.match_ahocorasick import run_ahocorasick
+from microtpct.core.match_algorithms.match_ahocorasick_rs import run_ahocorasick_rs
+# from microtpct.core.match_algorithms.match_blast_basic import run_blast
+# from microtpct.core.match_algorithms.match_blast import run_blast
+
+
+
+matching_results_1 = run_ahocorasick(target_db, query_db)
+matching_results_2 = run_ahocorasick_rs(target_db, query_db)
+#matching_results = run_blast(target_db, query_db)
+
+
+print(matching_results_1)
+print(matching_results_2)
+
+
+
+
+
+                    # Test des fonctions d'écriture des fichiers de sortie
+'''
+
+data = {
+    "micro_peptide_ID": ["mp_001", "mp_002", "mp_003"],
+    "protein_ID": ["prot_A", "prot_B", "prot_C"],
+    "peptide_seq": ["MKTLLV", "AGPQLR", "VVKSTA"],
+    "J_peptide_seq": ["MKTL-LV", "AGPQ-LR", "VVKS-TA"],
+    "start_position": [12, 45, 87],
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+write_micropeptides_to_csv(df) #,file_path="C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/MicroTPCT")
+write_metrics_to_csv(df) #,file_path="C:/Users/Ambre Bordas/Desktop/COURS/Specialisation/1-Projet_bioinfo/MicroTPCT")
+
+'''
+
+
+                                # Test du type de merge 
+'''
 
 algorithm_output = pd.DataFrame({
     "query_id": ["Q1", "Q2", "Q3", "Q4"],
@@ -75,3 +144,5 @@ print(output_data)
 output_data.columns = ['peptide_ID', 'protein_ID', 'protein_ID_X', 'start_position', 'peptide_seq', 'J_peptide_seq']
 print(output_data)
     
+'''
+
