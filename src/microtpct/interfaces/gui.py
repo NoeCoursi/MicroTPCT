be84d1ef47b1d9137ffc85f7f8f72ba2ccf0f6e5
AmbroiseLoeb <loeb.a@free.fr>
2026-01-27 23:34:53 +0100

    fix logo path for embeding in gui

--- src/microtpct/interfaces/gui.py
+++ src/microtpct/interfaces/gui.py
@@ -98,7 +98,7 @@ class MicroTPCTGUI:
         self.logo_label.pack(fill=tk.BOTH, expand=True)
 
         # Charger le logo PNG ici, après la création de left_frame
-        logo_path = "src/microtpct/interfaces/logo.png"
+        logo_path = "src/microtpct/assets/logoMu.png"
         logo_img = Image.open(logo_path)
 
         # Optionnel : créer l'image aux dimensions exactes de la frame pour éviter les resize dynamiques
